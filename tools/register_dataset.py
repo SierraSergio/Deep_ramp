f446e54ffda89ab022023746f28e182aad86d9c7
SierraSergio <58831974+SierraSergio@users.noreply.github.com>
2021-09-09 15:07:35 +0200

    subo todo

--- tools/register_dataset.py
+++
@@ -1,4 +0,0 @@
-from detectron2.data.datasets import register_coco_instances
-register_coco_instances("my_dataset_train", {}, "C:/Users/ser/Desktop/hasty_leptometras/coco_circarock.json", "C:/Users/ser/Desktop/hasty_leptometras")
-register_coco_instances("my_dataset_val", {}, "C:/Users/ser/Desktop/hasty_leptometras/coco_circarock.json", "C:/Users/ser/Desktop/hasty_leptometras")
-register_coco_instances("my_dataset_test", {}, "C:/Users/ser/Desktop/hasty_leptometras/coco_circarock.json", "C:/Users/ser/Desktop/hasty_leptometras")
